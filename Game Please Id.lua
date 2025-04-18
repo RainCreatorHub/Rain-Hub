9df0ffd264e469d1976c9356b461060cd4c7f054
RainCreatorHub <rain.hub.creator@gmail.com>
2025-04-18 07:02:52 -0300

    Update and rename Game Please Id to Game Please Id.lua

--- Game Please Id.lua
+++ Game Please Id.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/scriptList"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/scriptList.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
