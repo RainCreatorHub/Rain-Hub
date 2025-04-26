d81f5cc6e63505cdba98b15665ebd1628a60fa20
RainCreatorHub <rain.hub.creator@gmail.com>
2025-04-26 18:44:00 -0300

    Update scriptList.lua

--- scriptList.lua
+++ scriptList.lua
@@ -1,6 +1,6 @@
 local Games = {
   [83325092812472] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Tower%20Onli%20Wall%20Hop.lua", -- tower onli wall hop
-  [] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Just%20a%20Baseplate%20script.lua" -- Just a Baseplate
+  [17574618959] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Just%20a%20Baseplate%20script.lua" -- Just a Baseplate
 }
 
 return Games
