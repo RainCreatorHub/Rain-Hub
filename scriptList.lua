ee156d112b4d7840a888b987ebdb54d3aa0c9f95
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-01 09:29:04 -0300

    Update scriptList.lua

--- scriptList.lua
+++ scriptList.lua
@@ -1,6 +1,37 @@
-local Games = {
-  [83325092812472] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Tower%20Onli%20Wall%20Hop.lua", -- tower onli wall hop
-  [17574618959] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Just%20a%20Baseplate%20Select.lua" -- Just a Baseplate
-}
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 
-return Games
+-- Criar Janela
+local Window = redzlib:MakeWindow({
+  Title = "Rain hub | selector hub",
+  SubTitle = "by zaque_blox",
+  SaveFolder = ""
+})
+
+-- Botão minimize
+Window:AddMinimizeButton({
+    Button = { Image = "rbxassetid://", BackgroundTransparency = 0 },
+    Corner = { CornerRadius = UDim.new(0, 1) },
+})
+
+local InfoTab = Window:MakeTab({"Info", "Face"})
+local Section = InfoTab:AddSection({"Info"})
+
+local playerName = game.Players.LocalPlayer.Name
+local Paragraph = InfoTab:AddParagraph({"Seja Bem vindo(a) " .. playerName .. "!", ""})
+
+local scriptTab = Window:MakeTab({"script", "box"})
+local Section = scriptTab:AddSection({"scripts ( criado por mim )"})
+
+local Button = scriptTab:AddButton({
+    Title = "Rain hub ( Tower Onli Wall Hop )",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Tower%20Onli%20Wall%20Hop.lua"))()
+    end
+})
+
+local Button = scriptTab:AddButton({
+    Title = "Rain ( Just a Baseplate )",
+    Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Just%20a%20Baseplate%20Select.lua"))()
+    end
+})
