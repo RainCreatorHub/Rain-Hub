c73510f53f6567d15c8dc456f601608476738a1e
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-19 09:59:28 -0300

    Update Forsaken.lua

--- Forsaken.lua
+++ Forsaken.lua
@@ -1,6 +1,4 @@
--- Carrega a biblioteca OrionLibV2
-local p = "https://pastebin.com/raw/7pirbSGM"
-local OrionLibV2 = loadstring(game:HttpGet(p))()
+local OrionLibV2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/RainLibV2/refs/heads/main/OrionLibV2.lua"))()
 
 -- Criação da janela principal
 local window = OrionLibV2:MakeWindow({
