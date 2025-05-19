5d309c35e107743f44572842fdbc7ea0194cee08
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-19 10:35:20 -0300

    Update copy script.lua

--- copy script.lua
+++ copy script.lua
@@ -1,1 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Selector%20hub.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Selector%20hub.lua"))()
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
