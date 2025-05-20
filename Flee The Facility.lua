c249212aa6c21623355d1e9b4c5226e625016906
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-19 21:55:59 -0300

    Update Flee The Facility.lua

--- Flee The Facility.lua
+++ Flee The Facility.lua
@@ -1,1 +1,55 @@
-print("ainda sem nada...")
+local OrionLibV2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/OrionLibV2/refs/heads/main/OrionLibV2.lua"))()
+
+local window = OrionLibV2:MakeWindow({
+    Title = "Rain hub | Flee The Facility",
+    SubTitle = "by zaque_blox"
+})
+
+local InfoTab = window:MakeTab({
+    Name = "Info"
+})
+
+local InfoSection = InfoTab:AddSection({
+    Name = "Info"
+})
+
+local playerName = game.Players.LocalPlayer.Name
+
+local Label = Tab:AddLabel({
+    Name = "Bem vindo(a) " .. playerName .. "!",
+    Content = " "
+})
+
+local espTab = window:MakeTab({
+    Name = "esp"
+})
+
+local section = espTab:AddSection({
+    Name = "esp"
+})
+
+local espComputers = false
+local espComputers = {}
+
+function espComputersPlay()
+   espComputers = true
+end
+
+function espComputersStop()
+   espComputers = false
+end
+
+local ComputersEsp = espTab:AddToggle({
+    Name = "Computers",
+    Description = " ",
+    Default = false,
+    Callback = function(value)
+      if value == true then
+        espComputersPlay()
+      end
+      
+      if value == false then
+        espComputersStop()
+      end
+    end
+})
