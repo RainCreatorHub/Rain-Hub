5c60850dda48a1692eee1ec159ae5da8c7b9bfcd
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-20 01:16:35 -0300

    Update Selector hub.lua

--- Selector hub.lua
+++ Selector hub.lua
@@ -1,5 +1,6 @@
 local Games = {
   [18687417158] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Forsaken.lua", -- Forsaken
+  [893973440] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Flee%20The%20Facility.lua", -- Flee The Facility 
   [83325092812472] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Tower%20Onli%20Wall%20Hop.lua", -- Tower Onli wall hop
   [17574618959] = "https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Just%20a%20Baseplate%20Select.lua", -- Just a Baseplate 
 }
