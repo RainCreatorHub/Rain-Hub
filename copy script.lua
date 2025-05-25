bfcd57de0f2f6a13e1fb0681b5a8bd21a0dfb6dd
RainCreatorHub <rain.hub.creator@gmail.com>
2025-05-25 12:06:13 -0300

    Update copy script.lua

--- copy script.lua
+++ copy script.lua
@@ -1,7 +1,18 @@
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/RainCreatorHub/Rain-Hub/refs/heads/main/Selector%20hub.lua"))()
 
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
+if Games then
+    local found = false
+    for PlaceID, Execute in pairs(Games) do
+        if PlaceID == game.PlaceId then
+            loadstring(game:HttpGet(Execute))()
+            found = true
+            break
+        end
     end
+
+    if not found then
+        print("erro: entre e execute em um jogo válido")
+    end
+else
+    print("erro: carregar...")
 end
